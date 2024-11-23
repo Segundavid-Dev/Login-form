89046c4f7195a68ab311159ed2846a4409de7dcc
Segun David <segdavid03@gmail.com>
2024-11-23 11:26:20 +0100

    feat: toggle show password functionality for register account form

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,6 +1,6 @@
 "use strict";
 
-// show password login
+// show hide login password
 const passWordAccess = (loginPass, loginEye) => {
   const input = document.getElementById(loginPass);
   const iconEye = document.getElementById(loginEye);
@@ -19,3 +19,23 @@ const passWordAccess = (loginPass, loginEye) => {
 
 // call function
 passWordAccess("password", "loginPassword");
+
+// show hide Register password
+const passWordRegister = (loginPass, loginEye) => {
+  const input = document.getElementById(loginPass);
+  const iconEye = document.getElementById(loginEye);
+
+  iconEye.addEventListener("click", () => {
+    // change Password to text
+    input.type === "password"
+      ? (input.type = "text")
+      : (input.type = "password");
+
+    // icon change
+    iconEye.classList.toggle("ri-eye-fill");
+    iconEye.classList.toggle("ri-eye-off-fill");
+  });
+};
+
+// call function
+passWordRegister("passwordCreate", "loginPasswordCreate");
